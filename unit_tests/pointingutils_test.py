97913b74ed81bc03d9e818541c9a2688c2dc13bf
Brad Sease <bsease@vt.edu>
2017-03-26 17:02:31 -0400

    Cleaning up unit tests.
    worldobject_test and pointingutils_test

--- unit_tests/pointingutils_test.py
+++ unit_tests/pointingutils_test.py
@@ -1,83 +1,100 @@
+"""
+Pointingutils unit tests.
+"""
 import unittest
 import pointingutils as point
 import numpy as np
 
 class pointingutilstests(unittest.TestCase):
-    
+    """
+    Pointingutils unit test class.
+    """
     def setUp(self):
         pass
-        
+
     def tearDown(self):
         pass
 
 class test_rigid_body_kinematics(pointingutilstests):
+    """
+    Test rigid_body_kinematics function.
+    """
 
-	def test_output(self):
-	
+    def test_output(self):
+        """
+        Test output value and type.
+        """
+
 		# Set up inputs
-		quaternion   = np.array([0,0,0,1])
-		angular_rate = np.array([0,0,0])
-		
-		# Call method
-		deriv = point.rigid_body_kinematic(quaternion, angular_rate)
-		   
-		# Check dimensions and type
-		self.assertEqual(len(deriv), 7, 
-												 "Incorrect output dimension.")
-		self.assertTrue(type(deriv) is np.ndarray, "Incorrect output type.")
-													  
-		# Check values
-		self.assertTrue( np.array_equal(deriv, np.array([0,0,0,0,0,0,0])), 
-													 "Incorrect output value.")
-        
+        quaternion = np.array([0, 0, 0, 1])
+        angular_rate = np.array([0, 0, 0])
+
+        # Call method
+        deriv = point.rigid_body_kinematic(quaternion, angular_rate)
+
+        # Check dimensions and type
+        self.assertEqual(len(deriv), 7, "Incorrect output dimension.")
+        self.assertTrue(isinstance(deriv, np.ndarray), "Incorrect output type.")
+
+        # Check values
+        self.assertTrue(np.array_equal(deriv, np.array([0, 0, 0, 0, 0, 0, 0])),                    \
+                                                                          "Incorrect output value.")
+
 class test_quaternion2dcm(pointingutilstests):
+    """
+    Test quaternion to DCM conversion function.
+    """
 
     def test_types(self):
-        
-        # 
-        quaternion      = np.array([0,0,0,1])
-        quaternion_list = [ quaternion, quaternion, quaternion ]
-        
-        #
+        """
+        Test input / output types.
+        """
+
+        # Set up scenarios
+        quaternion = np.array([0, 0, 0, 1])
+        quaternion_list = [quaternion, quaternion, quaternion]
+
+        # Call function
         dcm_single = point.quaternion2dcm(quaternion)
-        dcm_multi  = point.quaternion2dcm(quaternion_list)
-        
-        #
-        self.assertEqual(type(dcm_single), np.ndarray, 
-                                                      "Incorrect output type.")
-        self.assertEqual(type(dcm_multi), list, "Incorrect output type." )
-    
+        dcm_multi = point.quaternion2dcm(quaternion_list)
+
+        # Check types
+        self.assertEqual(type(dcm_single), np.ndarray, "Incorrect output type.")
+        self.assertEqual(type(dcm_multi), list, "Incorrect output type.")
+
     def test_identity(self):
-    
-        # 
-        quaternion      = np.array([0,0,0,1])
-        quaternion_list = [ quaternion, quaternion, quaternion ]
-        
-        #
-        dcm_single      = point.quaternion2dcm(quaternion)
-        dcm_multi       = point.quaternion2dcm(quaternion_list)
-        
-        #
-        self.assertTrue((dcm_single == np.eye(3)).all(), 
-                                                  "Incorrect identity result.")
-        for dcm in dcm_multi:
-            self.assertTrue((dcm == np.eye(3)).all(), 
-                                                  "Incorrect identity result.")
-                                                  
-    def test_values(self):
-        
-        # 
-        quaternion      = np.array([0.5,0.5,0.5,0.5])
-        
-        #
+        """
+        Test identical assignment.
+        """
+
+        # Set up scenarios
+        quaternion = np.array([0, 0, 0, 1])
+        quaternion_list = [quaternion, quaternion, quaternion]
+
+        # Call function
         dcm_single = point.quaternion2dcm(quaternion)
-        
-        # 
-        result     = np.array([[0,0,1], [1,0,0], [0,1,0]])
-        
-        #
-        self.assertTrue((dcm_single == result).all(), 
-                                                     "Incorrect value result.")
+        dcm_multi = point.quaternion2dcm(quaternion_list)
+
+        # Check result
+        self.assertTrue((dcm_single == np.eye(3)).all(), "Incorrect identity result.")
+        for dcm in dcm_multi:
+            self.assertTrue((dcm == np.eye(3)).all(), "Incorrect identity result.")
+
+    def test_values(self):
+        """
+        Test values for a specific case.
+        """
+
+        # Set up scenario
+        quaternion = np.array([0.5, 0.5, 0.5, 0.5])
+
+        # Call function
+        dcm_single = point.quaternion2dcm(quaternion)
+
+        # Set up expected result
+        result = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+
+        # Check result
+        self.assertTrue((dcm_single == result).all(), "Incorrect value result.")
         for row in dcm_single:
-            self.assertTrue(np.isclose(np.linalg.norm(row),1),
-                                                "Incorrect dcm row magnitude.")
+            self.assertTrue(np.isclose(np.linalg.norm(row), 1), "Incorrect dcm row magnitude.")
--- unit_tests/worldobject_test.py
+++ unit_tests/worldobject_test.py
@@ -1,111 +1,131 @@
+"""
+WorldObject unit tests.
+"""
 import unittest
-import copy
 import worldobject as obj
 import numpy as np
 
 class worldobjecttests(unittest.TestCase):
-    
+    """
+    World object test class.
+    """
+
     def setUp(self):
         self.worldobject = obj.WorldObject()
-        
+
     def tearDown(self):
         del self.worldobject
 
-class test_init(worldobjecttests):
+class test_initialization(worldobjecttests):
+    """
+    Test world object initialization.
+    """
 
-	def test_types(self):
-		
-		# Check types
-		self.assertTrue( type(self.worldobject.quaternion) is np.ndarray,
-											 "Incorrect quaternion data type.")
-		self.assertTrue( type(self.worldobject.angular_rate) is np.ndarray,
-										  "Incorrect angular rate data type." )
-		self.assertTrue( type(self.worldobject.position) is np.ndarray,
-											  "Incorrect position data type." )
-		self.assertTrue( type(self.worldobject.velocity) is np.ndarray,
-											  "Incorrect velocity data type." )
-                                                     
+    def test_types(self):
+        """
+        Test for correct attribute types
+        """
+
+        # Check types
+        self.assertTrue(isinstance(self.worldobject.quaternion, np.ndarray),                      \
+                                                                 "Incorrect quaternion data type.")
+        self.assertTrue(isinstance(self.worldobject.angular_rate, np.ndarray),                    \
+                                                               "Incorrect angular rate data type.")
+        self.assertTrue(isinstance(self.worldobject.position, np.ndarray),                        \
+                                                                   "Incorrect position data type.")
+        self.assertTrue(isinstance(self.worldobject.velocity, np.ndarray),                        \
+                                                                   "Incorrect velocity data type.")
+
 class test_set_pointing_fcn(worldobjecttests):
+    """
+    Test set_pointing_fcn method.
+    """
 
     def test_ode(self):
-    
-        # Create "state" function
-        f = lambda t,state : [0,0,0,0,0,0,0]
-        
+        """
+        Test ODE input format.
+        """
+
+        # Create static "state" function
+        fcn = lambda t, state: [0, 0, 0, 0, 0, 0, 0]
+
         # Set function
-        self.worldobject.set_pointing_fcn(f, "ode")
-        
+        self.worldobject.set_pointing_fcn(fcn, "ode")
+
         # Check properties
-        self.assertEqual(self.worldobject.model_pointing, "on", 
-                                                  "Incorrect modeling option.")
-        self.assertEqual(self.worldobject.pointing_mode, "ode", 
-                                                    "Incorrect pointing mode.")
-                                                
+        self.assertEqual(self.worldobject.model_pointing, "on", "Incorrect modeling option.")
+        self.assertEqual(self.worldobject.pointing_mode, "ode", "Incorrect pointing mode.")
+
         # Check function output
-        test_result     = self.worldobject.pointing_fcn(1)
-        expected_result = np.hstack((self.worldobject.quaternion,
-                                                self.worldobject.angular_rate))
-        self.assertTrue(np.all(test_result == expected_result),
-                                                  "Incorrect function result.")
-                                                  
+        test_result = self.worldobject.pointing_fcn(1)
+        expected_result = np.hstack((self.worldobject.quaternion, self.worldobject.angular_rate))
+        self.assertTrue(np.all(test_result == expected_result), "Incorrect function result.")
+
     def test_explicit(self):
-    
+        """
+        Test explicit input format.
+        """
+
         # Create "state" function
-        f = lambda t : np.hstack((self.worldobject.quaternion,
-                                                self.worldobject.angular_rate))
-        
+        fcn = lambda t: np.hstack((self.worldobject.quaternion, self.worldobject.angular_rate))
+
         # Set function
-        self.worldobject.set_pointing_fcn(f, "explicit")
-        
+        self.worldobject.set_pointing_fcn(fcn, "explicit")
+
         # Check properties
-        self.assertEqual(self.worldobject.model_pointing, "on", 
-                                                  "Incorrect modeling option.")
-        self.assertEqual(self.worldobject.pointing_mode, "explicit", 
-                                                    "Incorrect pointing mode.")
-                                                
+        self.assertEqual(self.worldobject.model_pointing, "on", "Incorrect modeling option.")
+        self.assertEqual(self.worldobject.pointing_mode, "explicit", "Incorrect pointing mode.")
+
         # Check function output
-        test_result     = self.worldobject.pointing_fcn(1)
-        expected_result = np.hstack((self.worldobject.quaternion,
-                                                self.worldobject.angular_rate))
-        self.assertTrue(np.all(test_result == expected_result),
-                                                  "Incorrect function result.")
+        test_result = self.worldobject.pointing_fcn(1)
+        expected_result = np.hstack((self.worldobject.quaternion, self.worldobject.angular_rate))
+        self.assertTrue(np.all(test_result == expected_result), "Incorrect function result.")
 
 class test_set_pointing_preset(worldobjecttests):
+    """
+    Test set_pointing_preset method.
+    """
 
     def test_kinematic_preset(self):
-    
+        """
+        Test rigid body kinematic preset.
+        """
+
         # Set preset
         self.worldobject.set_pointing_preset("kinematic")
-        
+
         # Check function output
-        test_result     = self.worldobject.pointing_fcn(1)
-        expected_result = np.hstack((self.worldobject.quaternion,
-                                                self.worldobject.angular_rate))
-        self.assertTrue(np.all(test_result == expected_result),
-                                                  "Incorrect function result.")
-                                                  
+        test_result = self.worldobject.pointing_fcn(1)
+        expected_result = np.hstack((self.worldobject.quaternion, self.worldobject.angular_rate))
+        self.assertTrue(np.all(test_result == expected_result), "Incorrect function result.")
+
 class test_set_integrator(worldobjecttests):
+    """
+    Test set_integrator method.
+    """
 
     def test_set(self):
-        
+        """
+        Test basic integrator set.
+        """
         # Set integrator
         self.worldobject.set_integrator("vode", 1e-8, 1e-9)
-        
+
         # Check properties
-        self.assertEqual(self.worldobject.integrator, "vode", 
-                                                       "Incorrect integrator.")
-        self.assertEqual(self.worldobject.integrator_atol, 1e-8, 
-                                               "Incorrect absolute tolerance.")
-        self.assertEqual(self.worldobject.integrator_rtol, 1e-9, 
-                                               "Incorrect relative tolerance.")
-        
+        self.assertEqual(self.worldobject.integrator, "vode", "Incorrect integrator.")
+        self.assertEqual(self.worldobject.integrator_atol, 1e-8, "Incorrect absolute tolerance.")
+        self.assertEqual(self.worldobject.integrator_rtol, 1e-9, "Incorrect relative tolerance.")
+
     def test_set_with_pointing_ode(self):
-    
+        """
+        Test integrator set with a pointing ode that requires re-initialization.
+        """
+
         # Create "state" function
-        f = lambda t,state : [0,0,0,0,0,0,0]
-        
+        fcn = lambda t, state: [0, 0, 0, 0, 0, 0, 0]
+
         # Set function
-        self.worldobject.set_pointing_fcn(f, "ode")
-        
+        self.worldobject.set_pointing_fcn(fcn, "ode")
+
         # Set integrator
         self.worldobject.set_integrator("vode", 1e-8, 1e-9)
