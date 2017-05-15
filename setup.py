a114287be83a8fbe8beed9a20957b4f61244d6ad
Brad Sease <bsease@vt.edu>
2017-05-14 20:14:08 -0400

    Updated setup.py to include long description.

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+"""
+Astra Viso is a full-featured toolset for simulating stars and unresolved
+space objects. With this software it is simple to set up a simulated sensor
+and begin generating imagery immediately. The tools allow the user to switch
+between a number of commonly-used internal modeling presets easily while
+also having the flexibility to override internal modeling with externally-
+defined functions.
+"""
 from setuptools import setup, find_packages
+
+DOCLINES = (__doc__ or '').split("\n")
 
 setup(
 
     # Package definition
     name="astraviso",
-    version="0.1dev.2",
+    version="0.1.0",
     packages=find_packages(),
 
     # Required package data
@@ -20,8 +30,8 @@ setup(
     author="Brad Sease",
     author_email="bsease@vt.edu",
     description="A python simulator for star and unresolved spacecraft imaging.",
-    long_description="",
-    license="",
+    long_description="\n".join(DOCLINES[1:]),
+    license="MIT",
     keywords="star camera simulator astronomy astrometry",
     url="https://github.com/bradsease/astra-viso",
 
