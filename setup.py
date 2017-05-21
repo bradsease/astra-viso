a2954e739d6f88177bfc713e6ac6b9a780c0aaf4
Brad Sease <bsease@vt.edu>
2017-05-21 10:37:33 -0400

    Added classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -35,6 +35,32 @@ setup(
     keywords="star camera simulator astronomy astrometry",
     url="https://github.com/bradsease/astra-viso",
 
+    # Classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
     # Install settings
     zip_safe=True,
 
